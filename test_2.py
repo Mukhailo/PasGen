c182d000c5d5d23b820cc187315b1625a9aff3d0
Mukhailo <savcukmihajlo@gmail.com>
2021-03-21 20:17:23 +0200

    Update_1.1

---
+++ test_2.py
@@ -0,0 +1,45 @@
+# imports
+import random
+import subprocess, platform
+import colorama
+from colorama import Fore, Back, Style
+from logo import logo_add
+
+colorama.init()
+
+
+# logo()
+# def logo2():
+#     print(logo_add(),
+#         """
+#         1.Words + Nambers
+#         2.Symbols + Words + Nambers
+#         """)
+
+what = int(input('Enter namber: '))
+
+class Main():
+    
+    def __init__(self,char):
+        self.char = char
+
+    @staticmethod
+    def passw():
+        what1 = int(input(f'{Fore.RED}How many passwords do you want?: '))
+        what2 = int(input(f'{Fore.RED}What password length do you want?: '))
+        for i in range(what1):
+                passwords = ''
+                for x in range(what2):
+                    passwords += random.choice(char)
+                print('\n',passwords)
+
+gs = Main()
+
+if what == 1:
+    char = '+-/*!&$#?=@<>'
+    gs.passw(char)
+
+     
+
+if what > 4 or what < 1:
+    print('You have entered an incorrect number')
